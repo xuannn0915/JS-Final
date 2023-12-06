8e3efd846769f88139bcc58e219dad8c122d8776
xuannn0915 <xuannn0915@gmail.com>
2023-12-06 13:48:25 +0800

    img upload

--- js/swiper.js
+++ js/swiper.js
@@ -2,8 +2,21 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: "2.5",
   spaceBetween: 30,
   loop: true,
+  freeMode: true,
   autoplay: {
-    delay: 2500,
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: "2.5",
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  freeMode: true,
+  autoplay: {
+    delay: 4500,
     disableOnInteraction: false,
   },
 });
