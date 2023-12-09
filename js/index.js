b886569bf1efc5a048b4ced8f2db1ea877610aeb
xuan <xuannn0915@gmail.com>
2023-12-10 01:21:02 +0800

    加入購物車加入購物車api串接成功，準備進行選染購物車畫面

--- js/index.js
+++ js/index.js
@@ -2,69 +2,101 @@
 //UID:SilbCe0IbhXlbMHF9Eu6JUataJQ2
 
 const api_path = "xuan";
-const token ="SilbCe0IbhXlbMHF9Eu6JUataJQ2";
-const productList = document.querySelector('.productList');
-const productSelect = document.querySelector('#productSelect');
+const token = "SilbCe0IbhXlbMHF9Eu6JUataJQ2";
+const productList = document.querySelector(".productList");
+const productSelect = document.querySelector("#productSelect");
 
-let productData
+let productData;
 
-function init(){
-  axios.get(`https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/products`)
-  .then(function (response){
-    productData = response.data.products;
-    renderProductList(productData);
-  })
-  .catch(function (error){
-    console.log(error.response);
-  })
+function init() {
+  axios
+    .get(
+      `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/products`
+    )
+    .then(function (response) {
+      productData = response.data.products;
+      renderProductList(productData);
+    })
+    .catch(function (error) {
+      console.log(error.response);
+    });
 }
 
-function renderProductList(data){
-  let content='';
-  data.forEach(function (item){
+function renderProductList(data) {
+  let content = "";
+  data.forEach(function (item) {
     let str = `            
     <li class="col-3">
     <div class="card border-0 position-relative">
       <div class="text-white bg-black tag position-absolute">${item.category}</div>
       <img src=${item.images} alt="" class="card-pic">
-      <div class="btn text-white rounded-0 card-btn mb-2">加入購物車</div>
+      <input type="button" value="加入購物車" class="btn text-white rounded-0 card-btn mb-2" data-id="${item.id}">
       <div class="card-body p-0">
         <p class="productName mb-2">${item.title}</p>
         <p class="oldPrice text-decoration-line-through">NT${item.origin_price}</p>
         <p class="fs-3 newPrice">NT${item.price}</p>
       </div>
     </div>
-  </li>`
-  content+=str
-  })
-  productList.innerHTML=content;
+  </li>`;
+    content += str;
+  });
+  productList.innerHTML = content;
 }
 
-function selectProductList(e){
+function selectProductList(e) {
   e.preventDefault();
   let selectTag = e.target.value;
-  let selectAry=[];
-  if(selectTag==="全部"){
+  let selectAry = [];
+  if (selectTag === "全部") {
     renderProductList(productData);
-    return
-  }else{
-    productData.forEach(function(item){
-      let selectObj={};
-      if(selectTag===item.category){
-        selectObj.category= item.category;
-        selectObj.images= item.images;
-        selectObj.title= item.title;
-        selectObj.origin_price= item.origin_price;
-        selectObj.price= item.price;
-        selectAry.push(selectObj)
+    return;
+  } else {
+    productData.forEach(function (item) {
+      let selectObj = {};
+      if (selectTag === item.category) {
+        selectObj.category = item.category;
+        selectObj.images = item.images;
+        selectObj.title = item.title;
+        selectObj.origin_price = item.origin_price;
+        selectObj.price = item.price;
+        selectAry.push(selectObj);
+      } else {
+        console.log("查無此內容");
       }
-      else{
-        console.log('查無此內容');
-      }
-    })
-    renderProductList(selectAry)
+    });
+    renderProductList(selectAry);
   }
 }
 
-init()
-productSelect.addEventListener('change',selectProductList);
+function addShoppingCart(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "INPUT") {
+    return;
+  }
+  let productId = e.target.getAttribute("data-id");
+  // console.log(productId);
+  renderShoppingCart(productId);
+}
+
+function renderShoppingCart(id) {
+  let addCartObj = {};
+  addCartObj.productId = id;
+  addCartObj.quantity = 1
+  axios
+    .post(
+      `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`,
+      {
+        data:addCartObj
+      }
+    )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error.response);
+    });
+}
+
+init();
+productSelect.addEventListener("change", selectProductList);
+productList.addEventListener("click", addShoppingCart);
